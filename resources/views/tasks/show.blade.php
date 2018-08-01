3409a8ade8d7125960e68338dc2988807276ec3d
riku.kojima <rikuthekojima@gmail.com>
2018-08-01 10:39:59 +0000

    5th fixed

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    @if(Auth::check())
+    @if($task->user_id == Auth::id())
     <h1>{{ $task->id }}の詳細ページ</h1>
     <div class="row">
     <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
@@ -31,8 +31,9 @@
     @else
     <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to Tasklist</h1>
+                <h1>あなたはこのタスクに対応したユーザーではありません。</h1>
                 {{ link_to_route('signup.get','Sign up now!!',null , ['class' => 'btn btn-lg btn-primary']) }} 
+                {{ link_to_route('login','login',null , ['class' => 'btn btn-lg btn-primary']) }} 
             </div>
         </div>
     @endif
